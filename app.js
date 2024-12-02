65bcac72549482f21a3c4de845d5c4aed4c7c349
Spine <igniteboost.net@gmail.com>
2024-12-02 16:33:33 -0500

    Google sheets integration

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import {google} from 'googleapis';
 import * as user_service from './src_api/userService.js'
+import {authenticate} from '@google-cloud/local-auth';
+import path from 'path'
+
 
 import cors from 'cors'
 const app = express();
@@ -12,6 +15,91 @@ const port = 3000;
 // Mounting the app under the process.env.ROUTE as a parent route (if present)
 var route = process.env.ROUTE;
 var parent_app;
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+async function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E',
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  rows.forEach((row) => {
+    // Print columns A and E, which correspond to indices 0 and 4.
+    console.log(`${row[0]}, ${row[4]}`);
+  });
+}
 
 // Enable CORS for all routes
 app.use(cors());
@@ -52,3 +140,5 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
+
+authorize().then(listMajors).catch(console.error);
--- src_api/gameService.js
+++ src_api/gameService.js
@@ -4,11 +4,15 @@ import axios from 'axios';
 import * as config from '../config.js';
 import * as bcrypt from 'bcrypt-nodejs';
 import mongoose from 'mongoose';
-import { ObjectId } from 'mongodb'
-
+import { ObjectId } from 'mongodb';
 
 // TODO: Display the user's wrestlers in a table along with their points
 // This will be default to the user's landing page, but linking through the leaderboard
 // allows others to view that table
 
+
+// TODO: Show and update users based on the Google Sheet
+export const updatePoints = async function(req, res) {
+    // Check if the username isn't taken
+}
 // TODO: Show all of the user's teams in a leaderboard style
